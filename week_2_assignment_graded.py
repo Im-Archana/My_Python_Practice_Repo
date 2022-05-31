5bcc41444850397cc102405a1b267b185d9c6b3f
Im-Archana <106387855+Im-Archana@users.noreply.github.com>
2022-05-31 10:57:30 +0530

    Add files via upload
    Google IT Automation using Python Course 1 Week 2 graded assignment

---
+++ week_2_assignment_graded.py
@@ -0,0 +1,94 @@
+Week 2 Module Assignment
+
+def color_translator(color):
+	if color == "red":
+		hex_color = "#ff0000"
+	elif color == "green":
+		hex_color = "#00ff00"
+	elif color == "blue":
+		hex_color = "#0000ff"
+	else:
+		hex_color = "unknown"
+	return hex_color
+
+print(color_translator("blue")) # Should be #0000ff
+print(color_translator("yellow")) # Should be unknown
+print(color_translator("red")) # Should be #ff0000
+print(color_translator("black")) # Should be unknown
+print(color_translator("green")) # Should be #00ff00
+print(color_translator("")) # Should be unknown
+
+---------------------------------------------------------------------------
+
+
+def exam_grade(score):
+	if score>95:
+		grade = "Top Score"
+	elif score>=60:
+		grade = "Pass"
+	else:
+		grade = "Fail"
+	return grade
+
+print(exam_grade(65)) # Should be Pass
+print(exam_grade(55)) # Should be Fail
+print(exam_grade(60)) # Should be Pass
+print(exam_grade(95)) # Should be Pass
+print(exam_grade(100)) # Should be Top Score
+print(exam_grade(0)) # Should be Fail
+
+-------------------------------------------------------------------------------
+def format_name(first_name, last_name):
+	string="Name:" + str(first_name) +" " + str(last_name)
+	return string 
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
+
+-------------------------------------------------------------------------------
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
+
+---------------------------------------------------------------------------------
+
+def format_name(first_name, last_name):
+	if first_name=="" and last_name=="":
+		string=""
+	elif first_name=="":
+		string=last_name
+	elif last_name=="":
+		string=first_name
+	else:
+		string="Name: "+str(first_name)+","+str(last_name)
+	return string
+
+print(format_name("Ernest", "Hemingway"))
+# Should return the string "Name: Hemingway, Ernest"
+
+print(format_name("", "Madonna"))
+# Should return the string "Name: Madonna"
+
+print(format_name("Voltaire", ""))
+# Should return the string "Name: Voltaire"
+
+print(format_name("", ""))
+# Should return an empty string
